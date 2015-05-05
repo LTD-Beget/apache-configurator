f74cfee16b2f67ab486b45bdbaa4074f0512ccbb
ftp <voksiv_admin@nemo.beget.ru>
2015-05-05 13:16:57 +0300

    realized json array and plain serilexers

--- src/LTDBeget/ApacheConfigurator/Serializers/PlainSerializer.php
+++ src/LTDBeget/ApacheConfigurator/Serializers/PlainSerializer.php
@@ -10,9 +10,33 @@ namespace LTDBeget\ApacheConfigurator\Serializers;
 
 
 use LTDBeget\ApacheConfigurator\Interfaces\iConfigurationFile;
+use LTDBeget\ApacheConfigurator\Interfaces\iDirective;
 use LTDBeget\ApacheConfigurator\Interfaces\iSerializer;
 
-class PlainSerializer implements iSerializer {
+class PlainSerializer implements iSerializer
+{
+    protected static $instance = null;
+
+    protected function __construct()
+    {
+    }
+
+    protected function __clone()
+    {
+    }
+
+    /**
+     * singleton getter
+     * @return PlainSerializer
+     */
+    static protected function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
 
     /**
      *
@@ -21,7 +45,13 @@ class PlainSerializer implements iSerializer {
      */
     public static function serialize(iConfigurationFile $configurationFile)
     {
-        // TODO: Implement serialize() method.
+        $configurationPlain = "";
+
+        foreach($configurationFile->getInnerDirectives() as $directive) {
+            $configurationPlain .= self::getInstance()->directiveToPlain($directive);
+        }
+
+        return $configurationPlain;
     }
 
     /**
@@ -32,5 +62,28 @@ class PlainSerializer implements iSerializer {
     public static function deserialize($fileType, $configuration)
     {
         // TODO: Implement deserialize() method.
+    }
+
+    /**
+     * converts directive object to plain with its inner directives as plain too
+     * @param iDirective $directive
+     * @param int $nestLevel
+     * @return array
+     */
+    protected function directiveToPlain(iDirective $directive, $nestLevel = 0)
+    {
+        $tabulation     = $nestLevel > 0 ? str_repeat("  ", $nestLevel) : "";
+
+        if ($directive->isSection()) {
+            $directivePlain = $tabulation . "<{$directive->getType()} {$directive->getValue()}>\n";
+            foreach ($directive->getInnerDirectives() as $innerDirective) {
+                $directivePlain .= $this->directiveToPlain($innerDirective, $nestLevel + 1);
+            }
+            $directivePlain .= $tabulation . "</{$directive->getType()}>\n";
+        } else {
+            $directivePlain = $tabulation . "{$directive->getType()} {$directive->getValue()}\n";
+        }
+
+        return $directivePlain;
     }
 }
