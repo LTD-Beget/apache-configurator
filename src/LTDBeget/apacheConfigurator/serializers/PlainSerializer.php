2495ff0c709b2b8817d50cf4b120f0d7cbe4dd36
Voksiv <voksiv@gmail.com>
2015-05-29 17:58:49 +0300

    realized plain deserializer for plain apache configuration

--- src/LTDBeget/apacheConfigurator/serializers/PlainSerializer.php
+++ src/LTDBeget/apacheConfigurator/serializers/PlainSerializer.php
@@ -9,7 +9,9 @@
 namespace LTDBeget\apacheConfigurator\serializers;
 
 
+use LTDBeget\apacheConfigurator\ConfigurationFile;
 use LTDBeget\apacheConfigurator\interfaces\iConfigurationFile;
+use LTDBeget\apacheConfigurator\interfaces\iContext;
 use LTDBeget\apacheConfigurator\interfaces\iDirective;
 use LTDBeget\apacheConfigurator\interfaces\iSerializer;
 
@@ -61,8 +63,96 @@ class PlainSerializer implements iSerializer
      */
     public static function deserialize($fileType, $configuration)
     {
-        // TODO: Implement deserialize() method.
+        $configurationFile = new ConfigurationFile($fileType, []);
+        $directivesQueue = self::getInstance()->makeQueueOfDirectives($configuration);
+        self::getInstance()->processQueueOfDirectives($directivesQueue, $configurationFile, $configurationFile);
+
+        return $configurationFile;
     }
+
+    /**
+     * Make queury of directives, from given string configuration
+     * Ignore blank lines and comments lines
+     * @param String $configuration
+     * @return array
+     */
+    protected function makeQueueOfDirectives($configuration)
+    {
+        $queue = explode("\n", $configuration);
+
+        foreach($queue as $key => $vaule) {
+            $value = trim($vaule);
+            $queue[$key] = $value;
+
+            if($value == "") { // ignore blank
+                unset($queue[$key]);
+            }
+            if(substr($vaule, 0, 1) == "#") { // ignore comments
+                unset($queue[$key]);
+            }
+        }
+        return array_values($queue);
+    }
+
+    /**
+     * Addind directives from query in configurationFile
+     * @param Array $directivesArray
+     * @param ConfigurationFile $configurationFile
+     * @param iContext $context
+     */
+    protected function processQueueOfDirectives(array &$directivesArray, ConfigurationFile $configurationFile, iContext $context)
+    {
+        // start process all directives
+        while(count($directivesArray)) {
+            // get first in stack of directives
+            $plain_directive = array_shift($directivesArray);
+
+            //process directive syntx
+            // is section?
+            if(substr($plain_directive, 0, 1) == "<" and substr($plain_directive, strlen($plain_directive)-1, 1) == ">") {
+                $sectionDirective = true;
+                // is end of section?
+                if(substr($plain_directive, 0, 2) == "</") {
+                    $endOfSection = true;
+                } else {
+                    $endOfSection = false;
+                    // cut section delimeters
+                    $plain_directive = substr($plain_directive, 1, strlen($plain_directive)-2);
+                }
+            } else {
+                $sectionDirective = false;
+                $endOfSection = false;
+            }
+
+            if(!$endOfSection) {
+                // parse name and value of directive
+                $exploded_directive = explode(" ", $plain_directive);
+                $directiveName = array_shift($exploded_directive);
+                $directiveValue = implode(" ", $exploded_directive);
+                $directiveValue = trim($directiveValue);
+
+                // add directive in configurationFile
+                $directive = $configurationFile->addDirective(
+                    $directiveName,
+                    $directiveValue,
+                    $context
+                );
+
+                // if section, recursively process next directives in queue, while this section wouldn't ends
+                if($sectionDirective) {
+                    $this->processQueueOfDirectives($directivesArray, $configurationFile, $directive);
+                }
+
+            } else {
+                // if ends of section stop, this cycle
+                break;
+            }
+        }
+        // finaly stop process
+        return;
+    }
+
+
 
     /**
      * converts directive object to plain with its inner directives as plain too
